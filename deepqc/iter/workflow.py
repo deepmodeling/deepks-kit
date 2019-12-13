a32a85cacf7ba2b1efc01193156b82684b255452
Yixiao Chen <yixiaoc@princeton.edu>
2019-12-13 17:00:32 -0500

    refine workflow code structure

--- deepqc/iter/workflow.py
+++ deepqc/iter/workflow.py
@@ -3,6 +3,7 @@ import sys
 import shutil
 from copy import deepcopy
 from pathlib import Path
+from contextlib import nullcontext, redirect_stdout, redirect_stderr
 
 
 def link_file(src, dst):
@@ -21,6 +22,13 @@ def copy_file(src, dst):
         os.remove(dst)
         shutil.copy2(src, dst)
 
+def check_file_list(files):
+    if isinstance(files, str):
+        return [files]
+    if files is None:
+        return []
+    return files
+
 
 class AbstructStep(object):
     def __init__(self, workdir):
@@ -28,20 +36,9 @@ class AbstructStep(object):
         
     def __repr__(self):
         return f'<{type(self).__module__}.{type(self).__name__} with workdir: {self.workdir}>'
-    
-    def preprocess(self):
-        pass
-        
-    def execute(self, *args, **kwargs):
-        raise NotImplementedError
-        
-    def postprocess(self):
-        pass
         
     def run(self, *args, **kwargs):
-        self.preprocess()
-        self.execute(*args, **kwargs)
-        self.postprocess()
+        raise NotImplementedError
     
     def prepend_workdir(self, path):
         self.workdir = path / self.workdir
@@ -52,8 +49,8 @@ class AbstructStep(object):
 
 class AbstructTask(AbstructStep):
     def __init__(self, workdir='.', prev_task=None,
-                 prev_folder=None, require_prev_files=None, 
-                 share_folder='.', require_share_files=None):
+                 prev_folder=None, link_prev_files=None, copy_prev_files=None, 
+                 share_folder=None, link_share_files=None, copy_share_files=None):
         # workdir has to be relative in order to be chained
         # prev_task is dereferenced to folder dynamically.
         # folders are absolute.
@@ -61,19 +58,11 @@ class AbstructTask(AbstructStep):
         assert prev_task is None or prev_folder is None
         self.prev_task = prev_task
         self.prev_folder = Path(prev_folder).absolute() if prev_folder is not None else None
-        self.share_folder = Path(share_folder).absolute()
-
-        if isinstance(require_prev_files, str):
-            require_prev_files = [require_prev_files]
-        if require_prev_files is None:
-            require_prev_files = []
-        self.prev_files = require_prev_files
-        
-        if isinstance(require_share_files, str):
-            require_share_files = [require_share_files]
-        if require_share_files is None:
-            require_share_files = []
-        self.share_files = require_share_files
+        self.share_folder = Path(share_folder).absolute() if share_folder is not None else None
+        self.link_prev_files = check_file_list(link_prev_files)
+        self.copy_prev_files = check_file_list(copy_prev_files)
+        self.link_share_files = check_file_list(link_share_files)
+        self.copy_share_files = check_file_list(copy_share_files)
         
     def preprocess(self):
         if not self.workdir.exists():
@@ -82,16 +71,28 @@ class AbstructTask(AbstructStep):
             assert self.workdir.is_dir()
         if self.prev_folder is None:
             self.prev_folder = self.prev_task.workdir
-        for f in self.prev_files:
+        for f in self.link_prev_files:
             link_file(self.prev_folder / f, self.workdir / f)
-        for f in self.share_files:
+        for f in self.copy_prev_files:
+            copy_file(self.prev_folder / f, self.workdir / f)
+        for f in self.link_share_files:
             link_file(self.share_folder / f, self.workdir / f)
+        for f in self.copy_share_files:
+            copy_file(self.share_folder / f, self.workdir / f)
         self.olddir = os.getcwd()
         os.chdir(self.workdir)
     
+    def execute(self):
+        raise NotImplementedError
+
     def postprocess(self):
         os.chdir(self.olddir)
         
+    def run(self, *args, **kwargs):
+        self.preprocess()
+        self.execute()
+        self.postprocess()
+    
     def set_prev_task(self, task):
         assert isinstance(task, AbstructTask)
         self.prev_folder = None
@@ -99,33 +100,44 @@ class AbstructTask(AbstructStep):
 
 
 class BlankTask(AbstructTask):
-    def execute(self, *args, **kwargs):
+    def execute(self):
         pass
 
 
 class PythonTask(AbstructTask):
-    def __init__(self, pycallable, call_args=None, call_kwargs=None, **task_args):
+    def __init__(self, pycallable, 
+                 call_args=None, call_kwargs=None, 
+                 outlog=None, errlog=None,
+                 **task_args):
         super().__init__(**task_args)
         self.pycallable = pycallable
         self.call_args = call_args if call_args is not None else []
         self.call_kwargs = call_kwargs if call_kwargs is not None else {}
+        self.outlog = outlog
+        self.errlog = errlog
     
-    def execute(self, *args, **kwargs):
-        self.pycallable(*self.call_args, **self.call_kwargs)
+    def execute(self):
+        with (open(self.outlog, 'w') if self.outlog is not None 
+                else nullcontext(sys.stdout)) as fo, \
+             redirect_stdout(fo), \
+             (open(self.errlog, 'w') if self.errlog is not None 
+                else nullcontext(sys.stderr)) as fe, \
+             redirect_stderr(fe):
+            self.pycallable(*self.call_args, **self.call_kwargs)
 
 
 class Workflow(AbstructStep):
-    def __init__(self, child_tasks, workdir='.', logfile=None):
+    def __init__(self, child_tasks, workdir='.', record_file=None):
         super().__init__(workdir)
         self.child_tasks = [deepcopy(task) for task in child_tasks]
-        self.logfile = Path(logfile).absolute() if logfile is not None else None
+        self.record_file = Path(record_file).absolute() if record_file is not None else None
         for task in self.child_tasks:
             assert not task.workdir.is_absolute()
             task.prepend_workdir(self.workdir)
             if isinstance(task, Workflow):
-                task.logfile = self.logfile
+                task.record_file = self.record_file
         
-    def execute(self, parent_tag=(), restart_tag=None):
+    def run(self, parent_tag=(), restart_tag=None):
         start_idx = 0
         if restart_tag is not None:
             last_idx = restart_tag[0]
@@ -133,26 +145,26 @@ class Workflow(AbstructStep):
             if rest_tag:
                 last_tag = parent_tag+(last_idx,)
                 self.child_tasks[last_idx].run(last_tag, rest_tag)
-                self.write_log(last_tag)
+                self.write_record(last_tag)
             start_idx = last_idx + 1
         for i in range(start_idx, len(self.child_tasks)):
             curr_tag = parent_tag + (i,)
-            print('# starting task:', curr_tag) 
+            print('# starting step:', curr_tag) 
             task = self.child_tasks[i]
             task.run(curr_tag)
-            self.write_log(curr_tag)
+            self.write_record(curr_tag)
             
     def prepend_workdir(self, path):
         super().prepend_workdir(path)
         for task in self.child_tasks:
             task.prepend_workdir(path)
             
-    def write_log(self, tag):
-        if self.logfile is None:
+    def write_record(self, tag):
+        if self.record_file is None:
             return
         if isinstance(tag, (list, tuple)):
             tag = ' '.join(map(str,tag))
-        with self.logfile.open('a') as lf:
+        with self.record_file.open('a') as lf:
             lf.write(tag + '\n')
 
     def max_depth(self):
@@ -162,11 +174,11 @@ class Workflow(AbstructStep):
             return 1 + max(task.max_depth() for task in self.child_tasks if isinstance(task, Workflow))
             
     def restart(self):
-        with self.logfile.open() as lf:
+        with self.record_file.open() as lf:
             all_tags = [tuple(map(int, l.split())) for l in lf.readlines()]
         assert max(map(len, all_tags)) == self.max_depth()
         restart_tag = all_tags[-1]
-        print('# restart after task', restart_tag)
+        print('# restart after step', restart_tag)
         self.run((), restart_tag)
         
     def __getitem__(self, idx):
@@ -174,8 +186,9 @@ class Workflow(AbstructStep):
 
 
 class Sequence(Workflow):
-    def __init__(self, child_tasks, workdir='.', logfile=None, init_folder='.'):
-        super().__init__(child_tasks, workdir, logfile)
+    def __init__(self, child_tasks, workdir='.', record_file=None, init_folder='.'):
+        # would reset all tasks' prev folder into their prev task, except for the first one
+        super().__init__(child_tasks, workdir, record_file)
         self.chain_tasks()
         start = self.child_tasks[0]
         while isinstance(start, Workflow):
@@ -193,11 +206,11 @@ class Sequence(Workflow):
 
 
 class Iteration(Sequence):
-    def __init__(self, task, iternum, workdir='.', logfile=None, init_folder='.'):
+    def __init__(self, task, iternum, workdir='.', record_file=None, init_folder='.'):
         # iterated task should have workdir='.' to avoid redundant folders
         iter_tasks = [deepcopy(task) for i in range(iternum)]
         nd = len(str(iternum))
         for ii, itask in enumerate(iter_tasks):
             itask.prepend_workdir(f'iter.{ii:0>{nd}d}')
-        super().__init__(iter_tasks, workdir, logfile, init_folder)
+        super().__init__(iter_tasks, workdir, record_file, init_folder)
 
