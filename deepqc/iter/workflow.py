1e1f1bf0766773a35b2dccd755d997d2c1637a90
Yixiao Chen <yixiaoc@princeton.edu>
2019-12-19 23:39:37 -0500

    add batch and group batch tasks

--- deepqc/iter/workflow.py
+++ deepqc/iter/workflow.py
@@ -5,6 +5,7 @@ import subprocess as sp
 from copy import deepcopy
 from pathlib import Path
 from contextlib import nullcontext, redirect_stdout, redirect_stderr
+from .job.dispatcher import Dispatcher
 
 
 def link_file(src, dst):
@@ -45,12 +46,12 @@ def create_dir(dirname, backup=False):
     else:
         assert dirname.is_dir(), f'{dirname} is not a dir'
 
-def check_file_list(files):
-    if isinstance(files, str):
-        return [files]
-    if files is None:
+def check_arg_list(args):
+    if args is None:
         return []
-    return files
+    if not isinstance(args, list):
+        return [args]
+    return args
 
 def get_abs_path(p):
     if p is None:
@@ -89,10 +90,10 @@ class AbstructTask(AbstructStep):
         self.prev_task = prev_task
         self.prev_folder = get_abs_path(prev_folder)
         self.share_folder = get_abs_path(share_folder)
-        self.link_prev_files = check_file_list(link_prev_files)
-        self.copy_prev_files = check_file_list(copy_prev_files)
-        self.link_share_files = check_file_list(link_share_files)
-        self.copy_share_files = check_file_list(copy_share_files)
+        self.link_prev_files = check_arg_list(link_prev_files)
+        self.copy_prev_files = check_arg_list(copy_prev_files)
+        self.link_share_files = check_arg_list(link_share_files)
+        self.copy_share_files = check_arg_list(copy_share_files)
         
     def preprocess(self):
         create_dir(self.workdir, self.backup)
@@ -106,24 +107,29 @@ class AbstructTask(AbstructStep):
             link_file(self.share_folder / f, self.workdir / f)
         for f in self.copy_share_files:
             copy_file(self.share_folder / f, self.workdir / f)
-        self.olddir = os.getcwd()
-        os.chdir(self.workdir)
     
     def execute(self):
         raise NotImplementedError
 
     def postprocess(self):
-        os.chdir(self.olddir)
+        pass
         
     def run(self, *args, **kwargs):
         self.preprocess()
+        self.olddir = os.getcwd()
+        os.chdir(self.workdir)
         self.execute()
+        os.chdir(self.olddir)
         self.postprocess()
     
     def set_prev_task(self, task):
         assert isinstance(task, AbstructTask)
         self.prev_folder = None
         self.prev_task = task
+
+    def set_prev_folder(self, path):
+        self.prev_task = None
+        self.prev_folder = path
 
 
 class BlankTask(AbstructTask):
@@ -171,6 +177,97 @@ class ShellTask(AbstructTask):
             sp.run(self.cmd, env=self.env, shell=True, stdout=fo, stderr=fe)
 
 
+class BatchTask(AbstructTask):
+    def __init__(self, cmds, 
+                 dispatcher=None, resources=None, 
+                 outlog='log', errlog='err', 
+                 forward_files=None, backward_files=None,
+                 **task_args):
+        super().__init__(**task_args)
+        self.cmds = check_arg_list(cmds)
+        if dispatcher is None:
+            dispatcher = Dispatcher()
+        elif isinstance(dispatcher, dict):
+            dispatcher = Dispatcher(**dispatcher)
+        assert isinstance(dispatcher, Dispatcher)
+        self.dispatcher = dispatcher
+        self.resources = resources
+        self.outlog = outlog
+        self.errlog = errlog
+        self.forward_files = check_arg_list(forward_files)
+        self.backward_files = check_arg_list(backward_files)
+    
+    def execute(self):
+        tdict = self.make_dict(base=self.workdir)
+        self.dispatcher.run_jobs([tdict], group_size=1, work_path='.', 
+                                 resources=self.resources, forward_task_deref=True,
+                                 outlog=self.outlog, errlog=self.errlog)
+
+    def make_dict(self, base='.'):
+        return {'dir': str(self.workdir.relative_to(base)),
+                'cmds': self.cmds,
+                'forward_files': self.forward_files,
+                'backward_files': self.backward_files}
+
+
+class GroupBatchTask(AbstructTask):
+    # after grouping up, the following individual setting would be ignored:
+    # dispatcher, resources, outlog, errlog
+    # only grouped one setting in this task would be effective
+    def __init__(self, batch_tasks, group_size=1,
+                 dispatcher=None, resources=None, 
+                 outlog='log', errlog='err', common_files=None,
+                 **task_args):
+        super().__init__(**task_args)            
+        self.batch_tasks = [deepcopy(task) for task in batch_tasks]
+        for task in self.batch_tasks:
+            assert isinstance(task, BatchTask), f'given task is instance of {task.__class__}'
+            assert not task.workdir.is_absolute()
+            task.prepend_workdir(self.workdir)
+            if task.prev_folder is None:
+                task.prev_folder = self.prev_folder
+        self.group_size = group_size
+        if dispatcher is None:
+            dispatcher = Dispatcher()
+        elif isinstance(dispatcher, dict):
+            dispatcher = Dispatcher(**dispatcher)
+        assert isinstance(dispatcher, Dispatcher)
+        self.dispatcher = dispatcher
+        self.resources = resources
+        self.outlog = outlog
+        self.errlog = errlog
+        self.common_files = check_arg_list(common_files)
+
+    def execute(self):
+        tdicts = [t.make_dict(base=self.workdir) for t in self.batch_tasks]
+        self.dispatcher.run_jobs(tdicts, group_size=self.group_size, work_path='.', 
+                                 resources=self.resources, forward_task_deref=False,
+                                 forward_common_files=self.common_files,
+                                 outlog=self.outlog, errlog=self.errlog)
+    
+    def preprocess(self):
+        # if (self.workdir / 'fin.record').exists():
+        #     return
+        super().preprocess()
+        for t in self.batch_tasks:
+            t.preprocess()
+
+    def prepend_workdir(self, path):
+        super().prepend_workdir(path)
+        for t in self.batch_tasks:
+            t.prepend_workdir(path)
+    
+    def set_prev_task(self, task):
+        super().set_prev_task(task)
+        for t in self.batch_tasks:
+            t.set_prev_task(task)
+    
+    def set_prev_folder(self, path):
+        super().set_prev_folder(path)
+        for t in self.batch_tasks:
+            t.set_prev_task(path)
+
+
 class Workflow(AbstructStep):
     def __init__(self, child_tasks, workdir='.', record_file=None):
         super().__init__(workdir)
@@ -189,7 +286,7 @@ class Workflow(AbstructStep):
             rest_tag = restart_tag[1:]
             if rest_tag:
                 last_tag = parent_tag+(last_idx,)
-                self.child_tasks[last_idx].run(last_tag, rest_tag)
+                self.child_tasks[last_idx].run(last_tag, restart_tag=rest_tag)
                 self.write_record(last_tag)
             start_idx = last_idx + 1
         for i in range(start_idx, len(self.child_tasks)):
@@ -228,7 +325,7 @@ class Workflow(AbstructStep):
         # assert max(map(len, all_tags)) == self.max_depth()
         restart_tag = all_tags[-1]
         print('# restarting after step', restart_tag)
-        self.run((), restart_tag)
+        self.run((), restart_tag=restart_tag)
         
     def __getitem__(self, idx):
         return self.child_tasks[idx]
@@ -243,7 +340,7 @@ class Sequence(Workflow):
         while isinstance(start, Workflow):
             start = start.child_tasks[0]
         if start.prev_folder is None:
-            start.prev_folder = get_abs_path(init_folder)
+            start.set_prev_folder(get_abs_path(init_folder))
         
     def chain_tasks(self):    
         for prev, curr in zip(self.child_tasks[:-1], self.child_tasks[1:]):
