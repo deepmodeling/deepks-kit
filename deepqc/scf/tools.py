50c6fa435b438acf14db80c01294771c1ef38095
Yixiao Chen <yixiaoc@princeton.edu>
2020-08-22 21:28:13 -0400

    update post scf tools

--- deepqc/scf/tools.py
+++ deepqc/scf/tools.py
@@ -3,10 +3,134 @@ import sys
 import glob
 import numpy as np
 import shutil
-from deepqc.utils import check_array
+from deepqc.utils import check_list, check_array
+from deepqc.utils import load_array, get_with_prefix
 
 
-def print_stat(err, conv=None, train_idx=None, test_idx=None):
+def concat_data(systems=None, sys_dir=".", dump_dir=".", pattern="*"):
+    if systems is None:
+        systems = sorted(filter(os.path.isdir, map(os.path.abspath, glob.glob(f"{sys_dir}/*"))))
+    npy_names = list(map(os.path.basename, glob.glob(f"{systems[0]}/*.npy")))
+    os.makedirs(dump_dir, exist_ok=True)
+    for nm in npy_names:
+        tmp_array = np.concatenate([np.load(f"{sys}/{nm}") for sys in systems])
+        np.save(f"{dump_dir}/{nm}", tmp_array)
+    if os.path.exists(f'{systems[0]}/system.raw'):
+        shutil.copy(f'{systems[0]}/system.raw', dump_dir)
+
+
+def print_stat(train_sys=None, test_sys=None, 
+               group=False, dump_dir=".",
+               with_conv=True, with_e=True, e_name="e_cf", 
+               with_f=True, f_name="f_cf"):
+    load_func = load_stat if not group else load_stat_grouped
+    if train_sys:
+        tr_c, tr_e, tr_f = load_func(train_sys, dump_dir, with_conv, 
+                                     with_e, e_name, with_f, f_name)
+        print("Training:")
+        if with_conv:
+            print_stat_conv(tr_c, indent=2)
+        if with_e:
+            shift = tr_e.mean()
+            print_stat_e(tr_e, shift=shift, indent=2)
+        if with_f:
+            print_stat_f(tr_f, indent=2)
+    if test_sys and not group:
+        ts_c, ts_e, ts_f = load_func(test_sys, dump_dir, with_conv, 
+                                     with_e, e_name, with_f, f_name)
+        print("Testing:")
+        if with_conv:
+            print_stat_conv(ts_c, indent=2)
+        if with_e:
+            if not train_sys: shift = None
+            print_stat_e(ts_e, shift=shift, indent=2)
+        if with_f:
+            print_stat_f(ts_f, indent=2)
+    
+
+def print_stat_conv(conv, indent=0):
+    nsys = conv.shape[0]
+    ind = " "*indent
+    print(ind+f'Convergence:')
+    print(ind+f'  {np.sum(conv)} / {nsys} = \t {np.mean(conv):.5f}')
+
+
+def print_stat_e(e_err, shift=None, indent=0):
+    ind = " "*indent
+    print(ind+"Energy:")
+    print(ind+f'  ME: \t {e_err.mean()}')
+    print(ind+f'  MAE: \t {np.abs(e_err).mean()}')
+    if shift:
+        print(ind+f'  MARE: \t {np.abs(e_err-shift).mean()}')
+
+
+def print_stat_f(f_err, indent=0):
+    ind = " "*indent
+    print(ind+"Force:")
+    print(ind+f'  MAE: \t {np.abs(f_err).mean()}')
+
+
+def load_stat(systems, dump_dir=".",
+              with_conv=True, with_e=True, e_name="e_cf", 
+              with_f=True, f_name="f_cf"):
+    systems = check_list(systems)
+    c_res = []
+    e_err = []
+    f_err = []
+    for fl in systems:
+        lbase = fl.rstrip(os.path.sep).rstrip(".xyz")
+        rbase = os.path.join(dump_dir, os.path.basename(lbase))
+        if with_conv:
+            try:
+                c_res.append(load_array(get_with_prefix("conv", rbase, ".npy")))
+            except FileNotFoundError as e:
+                print("Warning! conv.npy not found:", e, file=sys.stderr)
+        if with_e:
+            try:
+                le = get_with_prefix("energy", lbase, ".npy")
+                re = get_with_prefix(e_name, rbase, ".npy")
+                e_err.append(load_array(le) - load_array(re))
+            except FileNotFoundError as e:
+                print("Warning! energy file not found:", e, file=sys.stderr)
+        if with_f:
+            try:
+                lf = get_with_prefix("force", lbase, ".npy")
+                rf = get_with_prefix(f_name, rbase, ".npy")
+                f_err.append(load_array(lf) - load_array(rf))
+            except FileNotFoundError as e:
+                print("Warning! force file not found:", e, file=sys.stderr)
+    return np.concatenate(c_res, 0) if with_conv else None, \
+           np.concatenate(e_err, 0) if with_e else None, \
+           np.concatenate(f_err, 0) if with_f else None
+
+
+def load_stat_grouped(systems, dump_dir=".",
+                      with_conv=True, with_e=True, e_name="e_cf", 
+                      with_f=True, f_name="f_cf"):
+    systems = check_list(systems)
+    lbases = [fl.rstrip(os.path.sep).rstrip(".xyz") for fl in systems]
+    if with_conv:
+        c_res = load_array(get_with_prefix("conv", dump_dir, ".npy"))
+    if with_e:
+        e_res = load_array(get_with_prefix(e_name, dump_dir, ".npy"))
+        e_lbl = np.concatenate([
+            load_array(get_with_prefix("energy", lb, ".npy")) for lb in lbases
+        ], 0)
+        e_err = e_lbl - e_res
+    if with_f:
+        f_res = load_array(get_with_prefix(f_name, dump_dir, ".npy"))
+        f_lbl = np.concatenate([
+            load_array(get_with_prefix("force", lb, ".npy")) for lb in lbases
+        ], 0)
+        f_err = f_lbl - f_res
+    return c_res if with_conv else None, \
+           e_err if with_e else None, \
+           f_err if with_f else None
+
+
+# Below are legacy tools, kept for old examples
+
+def print_stat_per_sys(err, conv=None, train_idx=None, test_idx=None):
     err = np.array(err).reshape(-1)
     nsys = err.shape[0]
     if conv is not None:
@@ -39,16 +163,6 @@ def make_label(sys_dir, eref, fref=None):
         np.save(f'{sys_dir}/l_f_delta.npy', fcc)
 
 
-def concat_data(sys_dir=".", dump_dir=".", pattern="*"):
-    systems = sorted(filter(os.path.isdir, map(os.path.abspath, glob.glob(f"{sys_dir}/*"))))
-    npy_names = list(map(os.path.basename, glob.glob(f"{systems[0]}/*.npy")))
-    os.makedirs(dump_dir, exist_ok=True)
-    for nm in npy_names:
-        tmp_array = np.concatenate([np.load(f"{sys}/{nm}") for sys in systems])
-        np.save(f"{dump_dir}/{nm}", tmp_array)
-    shutil.copy(f'{systems[0]}/system.raw', dump_dir)
-
-
 def collect_data(train_idx, test_idx=None, 
                  sys_dir="results", ene_ref="e_ref.npy", force_ref=None,
                  dump_dir=".", verbose=True):
@@ -76,7 +190,7 @@ def collect_data(train_idx, test_idx=None,
         test_idx = np.setdiff1d(np.arange(nsys), train_idx, assume_unique=True)
     if verbose:
         print(sys_dir)
-        print_stat(err, convs, train_idx, test_idx)
+        print_stat_per_sys(err, convs, train_idx, test_idx)
     
     np.savetxt(f'{dump_dir}/train_paths.raw', np.array(systems)[train_idx], fmt='%s')
     np.savetxt(f'{dump_dir}/test_paths.raw', np.array(systems)[test_idx], fmt='%s')
@@ -103,7 +217,7 @@ def collect_data_grouped(train_idx, test_idx=None,
         test_idx = np.setdiff1d(np.arange(nmol), train_idx, assume_unique=True)
     if verbose:
         print(sys_dir)
-        print_stat(err.reshape(-1), conv.reshape(-1), train_idx, test_idx)
+        print_stat_per_sys(err.reshape(-1), conv.reshape(-1), train_idx, test_idx)
     
     dd = [name for name in os.listdir(sys_dir) if ".npy" in name]
     os.makedirs(f'{sys_dir}/train', exist_ok=True)
