189dfc465802d7b337d4b67b2e27128575f390a7
Yixiao Chen <yixiaoc@princeton.edu>
2020-08-26 03:13:43 -0400

    refine test.py

--- deepqc/train/test.py
+++ deepqc/train/test.py
@@ -14,7 +14,7 @@ from deepqc.utils import load_yaml, load_sys_dirs, check_list
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def test(model, g_reader, dump_prefix="test"):
+def test(model, g_reader, dump_prefix="test", group=False):
     loss_fn=nn.MSELoss()
     label_list = []
     pred_list = []
@@ -33,10 +33,11 @@ def test(model, g_reader, dump_prefix="test"):
         label_list.append(label_np)
         pred_list.append(pred_np)
 
-        if dump_prefix is not None:
+        if not group and dump_prefix is not None:
+            nd = max(len(str(g_reader.nsystems)), 2)
             dump_res = np.stack([label_np, pred_np], axis=1)
             header = f"{g_reader.path_list[i]}\nmean l1 error: {error_l1}\nmean l2 error: {error_np}\nreal_ene  pred_ene"
-            filename = f"{dump_prefix}.{i}.out"
+            filename = f"{dump_prefix}.{i:0{nd}}.out"
             np.savetxt(filename, dump_res, header=header)
             # print(f"system {i} finished")
 
@@ -46,15 +47,15 @@ def test(model, g_reader, dump_prefix="test"):
     all_err_l2 = np.sqrt(np.mean((all_label - all_pred) ** 2))
     info = f"all systems mean l1 error: {all_err_l1}\nall systems mean l2 error: {all_err_l2}"
     print(info)
-    if dump_prefix is not None and g_reader.nsystems > 1:
-        np.savetxt(f"{dump_prefix}.all.out",
-                   np.stack([all_label, all_pred], axis=1), 
+    if dump_prefix is not None and group:
+        np.savetxt(f"{dump_prefix}.out", np.stack([all_label, all_pred], axis=1), 
                    header=info + "\nreal_ene  pred_ene")
     return all_err_l1, all_err_l2
 
 
 def main(data_paths, model_file="model.pth", 
-         output_prefix='test', e_name='l_e_delta', d_name=['dm_eig']):
+         output_prefix='test', group=False,
+         e_name='l_e_delta', d_name=['dm_eig']):
     data_paths = load_sys_dirs(data_paths)
     g_reader = GroupReader(data_paths, e_name=e_name, d_name=d_name)
     model_file = check_list(model_file)
@@ -66,7 +67,7 @@ def main(data_paths, model_file="model.pth",
         dir_name = os.path.dirname(dump)
         if dir_name:
             os.makedirs(dir_name, exist_ok=True)
-        test(model, g_reader, dump_prefix=dump)
+        test(model, g_reader, dump_prefix=dump, group=group)
 
 
 def cli():
@@ -85,6 +86,8 @@ def cli():
                         help="the name of energy file to be read (no .npy extension)")
     parser.add_argument("-D", "--d-name", type=str, nargs="+",
                         help="the name of descriptor file(s) to be read (no .npy extension)")
+    parser.add_argument("-G", "--group", action='store_true',
+                        help="group test results for all systems")
     args = parser.parse_args()
 
     if hasattr(args, "input"):
