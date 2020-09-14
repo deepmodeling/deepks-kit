d75c24ba286666f3108b6d90ac337deb612e80a5
Yixiao Chen <yixiaoc@princeton.edu>
2020-09-14 16:36:07 -0400

    update docstring

--- deepqc/iterate/main.py
+++ deepqc/iterate/main.py
@@ -21,59 +21,61 @@ def main(*args, **kwargs):
 
     Parameters
     ----------
-    systems_train: optional str or list of str 
+    systems_train: str or list of str, optional
         System paths used as training set in the procedure. These paths 
         can refer to systems or a file that contains multiple system paths.
-        Systems must be .xyz files or folder contains .npy files.
-        If given `None`, use ``$share_folder/systems_train.raw`` as default.
-    systems_test: optional str or list of str
+        Systems must be .xyz files or folders contains .npy files.
+        If not given, use ``$share_folder/systems_train.raw`` as default.
+    systems_test: str or list of str, optional
         System paths used as testing (or validation) set in the procedure. 
-        The format is same as `systems_train`. If given `None`, use the last
+        The format is same as `systems_train`. If not given, use the last
         system in the training set as testing system.
-    n_iter: int
-        The number of iterations to do. Default is 5.
-    workdir: str
+    n_iter: int, optional
+        The number of iterations to do. Default is 0.
+    workdir: str, optional
         The working directory. Default is current directory (`.`).
-    share_folder: str
+    share_folder: str, optional
         The folder to store shared files in the iteration, including
         ``scf_input.yaml``, ``train_input.yaml``, and possibly files for
         initialization. Default is ``share``.
-    scf_input: bool or str or dict
-        Arguments used to specify the SCF calculation. If given `None` or 
-        `False`, use program default (unreliable). Otherwise, the arguments 
-        would be saved as a YAML file at ``$share_folder/scf_input.yaml``
-        and used for SCF calculation. If given `True`, use the existing file.
+    scf_input: bool or str or dict, optional
+        Arguments used to specify the SCF calculation. If given `None` or
+        `False`, bypass the checking and use program default (unreliable). 
+        Otherwise, the arguments would be saved as a YAML file at 
+        ``$share_folder/scf_input.yaml`` and used for SCF calculation. 
+        Default is `True`, which will check and use the existing file.
         If given a string of file path, copy the corresponding file into 
         target location. If given a dict, dump it into the target file.
-    scf_machine: optional str or dict
+    scf_machine: str or dict, optional
         Arguments used to specify the job settings of SCF calculation,
         including submitting method, resources, group size, etc..
         If given a string of file path, load that file as a dict using 
-        YAML format. If `strict` is set to false, additional arguments
-        can be passed to `Task` constructor to do more customization.
-    train_input: bool or str or dict
+        YAML format. If not given, using program default setup.
+    train_input: bool or str or dict, optional 
         Arguments used to specify the training of neural network. 
         It follows the same rule as `scf_input`, only that the target 
         location is ``$share_folder/train_input.yaml``.
-    train_machine: optional str or dict
+    train_machine: str or dict, optional 
         Arguments used to specify the job settings of NN training. 
         It Follows the same rule as `scf_machine`, but without group.
-    init_model: bool or str
+    init_model: bool or str, optional 
         Decide whether to use an existing model as the starting point.
-        If set to `True`, look for a model at ``$share_folder/init/model.pth``
-        If set to `False`, use `init_scf` and `init_train` to run an
-        extra initialization iteration in folder ``iter.init``. 
+        If set to `False` (default), use `init_scf` and `init_train` 
+        to run an extra initialization iteration in folder ``iter.init``. 
+        If set to `True`, look for a model at ``$share_folder/init/model.pth``.
         If given a string of path, copy that file into target location.
-    init_scf: bool or str or dict
+    init_scf: bool or str or dict, optional 
         Similar to `scf_input` but used for init calculation. The target
-        location is ``$share_folder/init_scf.yaml``.
-    init_train: bool or str or dict
+        location is ``$share_folder/init_scf.yaml``. Defaults to True.
+    init_train: bool or str or dict, optional 
         Similar to `train_input` but used for init calculation. The target
-        location is ``$share_folder/init_train.yaml``.
-    cleanup: bool
-        Whether to remove job files during calculation, such as `slurm-*.out`.
-    strict: bool
-        Whether to allow additional arguments to be passed to task constructor.
+        location is ``$share_folder/init_train.yaml``. Defaults to True.
+    cleanup: bool, optional 
+        Whether to remove job files during calculation, 
+        such as ``slurm-*.out`` and ``err``. Defaults to False.
+    strict: bool, optional 
+        Whether to allow additional arguments to be passed to task constructor,
+        through `scf_machine` and `train_machine`. Defaults to True.
 
     Returns
     -------
