35d333d73debac4f5c2f4cd9f545b2d8f098a6ec
Yixiao Chen <yixiaoc@princeton.edu>
2020-12-15 14:05:44 -0500

    add example for density training

---
+++ examples/water_single/withdens/pipe.sh
@@ -0,0 +1,2 @@
+python -u -m deepqc iterate base.yaml penalty.yaml >> log.iter 2> err.iter &&\
+python -u -m deepqc iterate base.yaml relax.yaml >> log.iter 2> err.iter
---
+++ examples/water_single/withdens/run.sh
@@ -0,0 +1,2 @@
+nohup bash pipe.sh >/dev/null 2>&1 &
+echo $! > PID
