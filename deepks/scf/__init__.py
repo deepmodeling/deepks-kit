14a6f29d1b545cc9b0a0e795e2eccdecf2608c37
Yixiao Chen <yixiaoc@princeton.edu>
2020-12-26 00:03:51 -0500

    warpper func deepks.scf.DSCF

--- deepks/scf/__init__.py
+++ deepks/scf/__init__.py
@@ -12,3 +12,14 @@ def __getattr__(name):
     if name in __all__:
         return import_module("." + name, __name__)
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def DSCF(mol, model, xc="HF", **kwargs):
+    """A wrap function to create NN SCF object (RDSCF or UDSCF)"""
+    from .scf import RDSCF, UDSCF
+    if mol.spin == 0:
+        return RDSCF(mol, model, xc, **kwargs)
+    else:
+        return UDSCF(mol, model, xc, **kwargs)
+
+DeepSCF = DSCF
