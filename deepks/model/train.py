c87d071c2598f807d62deeff79b22da4bc888099
Yixiao Chen <yixiaoc@princeton.edu>
2021-01-06 20:08:53 -0500

    add support for gradient penalty

--- deepks/model/train.py
+++ deepks/model/train.py
@@ -26,22 +26,39 @@ def calc_force(ene, eig, gvx):
     return force
 
 
-def make_evaluator(force_factor=0, loss_fn=nn.MSELoss(), device=DEVICE):
+DEFAULT_LOSS = nn.MSELoss(reduction="sum")
+
+def make_evaluator(force_factor=0., grad_penalty=0., loss_fn=DEFAULT_LOSS, device=DEVICE):
     # make evaluator a closure to save parameters
     def evaluator(model, sample):
         # allocate data first
+        part_loss = []
+        tot_loss = 0.
         e_label, eig, *force_sample = [d.to(device, non_blocking=True) for d in sample]
-        if force_factor > 0:
+        nframe = e_label.shape[0]
+        if force_factor > 0 or grad_penalty > 0:
             eig.requires_grad_(True)
         # begin the calculation
         e_pred = model(eig)
-        loss = loss_fn(e_pred, e_label)
+        e_loss = loss_fn(e_pred, e_label) / nframe
+        part_loss.append(e_loss)
+        tot_loss = tot_loss + e_loss
+        if force_factor > 0 or grad_penalty > 0:
+            [gev] = torch.autograd.grad(e_pred, eig, 
+                        grad_outputs=torch.ones_like(e_pred),
+                        retain_graph=True, create_graph=True, only_inputs=True)
+            if grad_penalty > 0:
+                # this does not enter into partloss since it is a penalty
+                gp_loss = gev.square().sum() / nframe
+                tot_loss = tot_loss + grad_penalty * gp_loss
         # optional force calculation
-        if force_factor > 0:
-            f_label, gvx = force_sample
-            f_pred = calc_force(e_pred, eig, gvx)
-            loss = loss + force_factor * loss_fn(f_pred, f_label)
-        return loss
+            if force_factor > 0:
+                f_label, gvx = force_sample
+                f_pred = - torch.einsum("...bxap,...ap->...bx", gvx, gev)
+                f_loss = loss_fn(f_pred, f_label) / nframe
+                part_loss.append(f_loss)
+                tot_loss = tot_loss + force_factor * f_loss
+        return (tot_loss, *part_loss)
     # return the closure
     return evaluator
 
@@ -72,9 +89,11 @@ def preprocess(model, g_reader,
 
 
 def train(model, g_reader, n_epoch=1000, 
-          test_reader=None, force_factor=0,
-          start_lr=0.001, decay_steps=100, decay_rate=0.96, stop_lr=None,
-          weight_decay=0.0, display_epoch=100, ckpt_file="model.pth", device=DEVICE):
+          test_reader=None, force_factor=0.,
+          start_lr=0.001, decay_steps=100, 
+          decay_rate=0.96, stop_lr=None,
+          weight_decay=0., grad_penalty=0.,
+          display_epoch=100, ckpt_file="model.pth", device=DEVICE):
     
     model = model.to(device)
     model.eval()
@@ -87,13 +106,15 @@ def train(model, g_reader, n_epoch=1000,
         print(f"# resetting decay_rate: {decay_rate:.4f} "
               + f"to satisfy stop_lr: {stop_lr:.2e}")
     scheduler = optim.lr_scheduler.StepLR(optimizer, decay_steps, decay_rate)
-    evaluator = make_evaluator(force_factor, nn.MSELoss(), device)
+    # evaluator returns a list of [tot_loss, *part_loss]
+    # where part_loss = [e_loss, f_loss, ...] if they present.
+    evaluator = make_evaluator(force_factor, grad_penalty, DEFAULT_LOSS, device)
 
     print("# epoch      trn_err   tst_err        lr  trn_time  tst_time ")
     tic = time()
-    trn_loss = np.mean([evaluator(model, batch).item() 
+    trn_loss = np.mean([evaluator(model, batch)[1].item() 
                     for batch in g_reader.sample_all_batch()])
-    tst_loss = np.mean([evaluator(model, batch).item() 
+    tst_loss = np.mean([evaluator(model, batch)[1].item() 
                     for batch in test_reader.sample_all_batch()])
     tst_time = time() - tic
     print(f"  {0:<8d}  {np.sqrt(trn_loss):>.2e}  {np.sqrt(tst_loss):>.2e}"
@@ -105,7 +126,7 @@ def train(model, g_reader, n_epoch=1000,
         for sample in g_reader:
             model.train()
             optimizer.zero_grad()
-            loss = evaluator(model, sample)
+            loss = evaluator(model, sample)[0]
             loss.backward()
             optimizer.step()
             loss_list.append(loss.item())
@@ -116,7 +137,7 @@ def train(model, g_reader, n_epoch=1000,
             trn_loss = np.mean(loss_list)
             trn_time = time() - tic
             tic = time()
-            tst_loss = np.mean([evaluator(model, batch).item() 
+            tst_loss = np.mean([evaluator(model, batch)[1].item() 
                             for batch in test_reader.sample_all_batch()])
             tst_time = time() - tic
             print(f"  {epoch:<8d}  {np.sqrt(trn_loss):>.2e}  {np.sqrt(tst_loss):>.2e}"
