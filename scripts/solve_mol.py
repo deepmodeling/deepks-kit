35ae7a9133b599d9b6e22abee8526436e35ad734
Yixiao Chen <yixiaoc@princeton.edu>
2021-01-29 00:19:25 -0500

    support force uhf in script

--- scripts/solve_mol.py
+++ scripts/solve_mol.py
@@ -10,6 +10,10 @@ from pyscf import gto, scf
 
 BOHR = 0.52917721092
 
+_NO_FORCE = False
+_NO_DM = False
+_MUST_UNRES = False
+
 
 def parse_xyz(filename, basis='ccpvdz', **kwargs):
     with open(filename) as fp:
@@ -37,72 +41,78 @@ def get_method(name: str):
     if lname == "ccsd":
         return calc_ccsd
     if lname.startswith(("ccsd_t", "ccsd-t", "ccsd(t)")):
-        if lname.endswith(("nof", "noforce")):
-            return lambda mol, **scfargs: calc_ccsd_t(mol, False, **scfargs)
         return calc_ccsd_t
     if lname == "fci":
         return calc_fci
     raise ValueError(f"Unknown calculation method: {name}")
 
 def calc_hf(mol, **scfargs):
-    mf = scf.HF(mol).run(**scfargs)
+    HFmethod = scf.HF if not _MUST_UNRES else scf.UHF
+    mf = HFmethod(mol).run(**scfargs)
     if not mf.converged:
         raise RuntimeError("SCF not converged!")
     etot = mf.e_tot
-    grad = mf.nuc_grad_method().kernel()
-    rdm = mf.make_rdm1()
-    return etot, -grad/BOHR, rdm
+    grad = mf.nuc_grad_method().kernel() if not _NO_FORCE else None
+    rdm = mf.make_rdm1() if not _NO_DM else None
+    return etot, grad, rdm
 
 def calc_dft(mol, xc="pbe", **scfargs):
     from pyscf import dft
-    mf = dft.KS(mol, xc).run(**scfargs)
+    KSmethod = dft.KS if not _MUST_UNRES else dft.UKS
+    mf = KSmethod(mol, xc).run(**scfargs)
     if not mf.converged:
         raise RuntimeError("SCF not converged!")
     etot = mf.e_tot
-    rdm = mf.make_rdm1()
-    if dft.libxc.xc_type(xc) in ('MGGA', 'NLC'):
-        return etot, None, rdm
-    grad = mf.nuc_grad_method().kernel()
-    return etot, -grad/BOHR, rdm
+    if _NO_FORCE or dft.libxc.xc_type(xc) in ('MGGA', 'NLC'):
+        grad = None
+    else:
+        grad = mf.nuc_grad_method().kernel()
+    rdm = mf.make_rdm1() if not _NO_DM else None
+    return etot, grad, rdm
 
 def calc_mp2(mol, **scfargs):
     import pyscf.mp
-    mf = scf.HF(mol).run(**scfargs)
+    HFmethod = scf.HF if not _MUST_UNRES else scf.UHF
+    mf = HFmethod(mol).run(**scfargs)
     if not mf.converged:
         raise RuntimeError("SCF not converged!")
     postmf = pyscf.mp.MP2(mf).run()
     etot = postmf.e_tot
-    grad = postmf.nuc_grad_method().kernel()
-    return etot, -grad/BOHR, None
+    grad = postmf.nuc_grad_method().kernel() if not _NO_FORCE else None
+    return etot, grad, None
 
 def calc_ccsd(mol, **scfargs):
     import pyscf.cc
-    mf = scf.HF(mol).run(**scfargs)
+    HFmethod = scf.HF if not _MUST_UNRES else scf.UHF
+    mf = HFmethod(mol).run(**scfargs)
     if not mf.converged:
         raise RuntimeError("SCF not converged!")
     mycc = mf.CCSD().run()
     etot = mycc.e_tot
-    grad = mycc.nuc_grad_method().kernel()
-    ccdm = np.einsum('...pi,...ij,...qj->...pq', mf.mo_coeff, mycc.make_rdm1(), mf.mo_coeff.conj())
-    return etot, -grad/BOHR, ccdm
+    grad = mycc.nuc_grad_method().kernel() if not _NO_FORCE else None
+    ccdm = np.einsum('...pi,...ij,...qj->...pq', 
+        mf.mo_coeff, mycc.make_rdm1(), mf.mo_coeff.conj()) if not _NO_DM else None
+    return etot, grad, ccdm
 
-def calc_ccsd_t(mol, calc_force=True, **scfargs):
+def calc_ccsd_t(mol, **scfargs):
     import pyscf.cc
-    mf = scf.HF(mol).run(**scfargs)
+    HFmethod = scf.HF if not _MUST_UNRES else scf.UHF
+    mf = HFmethod(mol).run(**scfargs)
     if not mf.converged:
         raise RuntimeError("SCF not converged!")
     mycc = mf.CCSD().run()
     et_correction = mycc.ccsd_t()
     etot = mycc.e_tot + et_correction
-    if not calc_force:
+    if _NO_FORCE:
         return etot, None, None
     import pyscf.grad.ccsd_t as ccsd_t_grad
     grad = ccsd_t_grad.Gradients(mycc).kernel()
-    return etot, -grad/BOHR, None
+    return etot, grad, None
 
 def calc_fci(mol, **scfargs):
     import pyscf.fci
-    mf = scf.HF(mol).run(**scfargs)
+    HFmethod = scf.HF if not _MUST_UNRES else scf.UHF
+    mf = HFmethod(mol).run(**scfargs)
     if not mf.converged:
         raise RuntimeError("SCF not converged!")
     myci = pyscf.fci.FCI(mf)
@@ -120,9 +130,16 @@ if __name__ == "__main__":
     parser.add_argument("-B", "--basis", default="ccpvdz", type=str, help="basis used to do the calculation")
     parser.add_argument("-C", "--charge", default=0, type=int, help="net charge of the molecule")
     parser.add_argument("-M", "--method", default="ccsd", help="method used to do the calculation. support MP2, CCSD and CCSD(T)")
+    parser.add_argument("-U", "--unrestrict", action="store_true", help="force using unrestricted methods")
+    parser.add_argument("-NF", "--no-force", action="store_true", help="do not calculate force")
+    parser.add_argument("-ND", "--no-dm", action="store_true", help="do not calculate dm")
     parser.add_argument("--scf-input", help="yaml file to specify scf arguments")
     args = parser.parse_args()
     
+    if args.unrestrict: _MUST_UNRES = True
+    if args.no_force: _NO_FORCE = True
+    if args.no_dm: _NO_DM = True
+
     scfargs = {}
     if args.scf_input is not None:
         import ruamel.yaml as yaml
@@ -140,14 +157,15 @@ if __name__ == "__main__":
         except RuntimeError as err:
             print(fn, f"failed, {err}")
             continue
-        etot, force, rdm = res
+        etot, grad, rdm = res
         if args.dump_dir is None:
             dump_dir = os.path.dirname(fn)
         else:
             dump_dir = args.dump_dir
         dump = os.path.join(dump_dir, os.path.splitext(os.path.basename(fn))[0])
         np.save(dump+".energy.npy", [etot])
-        if force is not None:
+        if grad is not None:
+            force = -grad / BOHR
             np.save(dump+".force.npy", force)
         if rdm is not None:
             np.save(dump+".dm.npy", rdm)
