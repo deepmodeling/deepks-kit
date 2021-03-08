a62e5c782a47466151b8834d98a3bc36f61af708
Yixiao Chen <yixiaoc@princeton.edu>
2021-03-07 23:24:18 -0500

    fix save and load basis

--- deepks/utils.py
+++ deepks/utils.py
@@ -17,6 +17,7 @@ _zeta = 1.5**np.array([17,13,10,7,5,3,2,1,0,-1,-2,-3])
 _coef = np.diag(np.ones(_zeta.size)) - np.diag(np.ones(_zeta.size-1), k=1)
 _table = np.concatenate([_zeta.reshape(-1,1), _coef], axis=1)
 DEFAULT_BASIS = [[0, *_table.tolist()], [1, *_table.tolist()], [2, *_table.tolist()]]
+DEFAULT_SYMB = "Ne"
 
 def load_basis(basis):
     if basis is None:
@@ -29,17 +30,21 @@ def load_basis(basis):
         table = np.load(basis)
         return [[ll, *table.tolist()] for ll in range(3)]
     elif basis.endswith(".npz"):
-        all_tables = np.load(basis).values()
-        return [[ll, *table.tolist()] for ll, table in enumerate(all_tables)]
+        all_tables = np.load(basis)
+        return [[int(name.split("_L")[-1]) if "_L" in name else ii, *table.tolist()] 
+                for ii, (name, table) in enumerate(all_tables.items())]
     else:
         from pyscf import gto
-        return gto.basis.load(basis, symb="Ne")
+        symb = DEFAULT_SYMB
+        if "@" in basis:
+            basis, symb = basis.split("@")
+        return gto.basis.load(basis, symb=symb)
 
 
 def save_basis(file, basis):
     """Save the basis to npz file from internal format of pyscf"""
-    tables = [np.array(b[1:]) for b in basis]
-    np.savez(file, *tables)
+    tables = {f"arr_{i}_L{l}":np.array(b) for i, (l,*b) in enumerate(basis)}
+    np.savez(file, **tables)
 
 
 def get_shell_sec(basis):
