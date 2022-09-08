78fd351bd607e198d5dd6167750a3ecdbd4cdcbe
ouqi0711 <ouqi@sas.upenn.edu>
2022-09-08 10:49:23 +0800

    yaml files in water_single_lda2pbe_abacus fixed

--- examples/water_single_lda2pbe_abacus/iter/run_bohrium.sh
+++ examples/water_single_lda2pbe_abacus/iter/run_bohrium.sh
@@ -1,2 +1,2 @@
-nohup python -u -m deepks iterate machines_bohrium.yaml params.yaml systems.yaml scf_abacus_bohrium.yaml >> log.iter 2> err.iter & 
+nohup python -u -m deepks iterate machines_bohrium.yaml params.yaml systems.yaml scf_abacus.yaml >> log.iter 2> err.iter & 
 echo $! > PID
